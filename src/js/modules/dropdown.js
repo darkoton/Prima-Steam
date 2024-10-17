8018c05ea0f3aced8fc020819655498bacf4bcd3
darkoto12 <darkoto12@gmail.com>
2024-10-18 01:10:11 +0300

    update dropdown

--- src/js/modules/dropdown.js
+++ src/js/modules/dropdown.js
@@ -1,6 +1,6 @@
 const dropdowns = Array.from(document.querySelectorAll('.header__nav-dropdown'));
 
-const activeList = ['!z-[5]', '!opacity-100', 'translate-y-[-4px]'];
+const activeList = ['!z-[5]', '!opacity-100'];
 
 const activeIcon = ['!-scale-y-100'];
 
@@ -18,22 +18,26 @@ const dropdownsData = dropdowns.map(dropdown => {
 });
 
 dropdownsData.forEach(data => {
-  data.button.addEventListener('mouseover', () => {
+  data.button.addEventListener('mouseenter', () => {
     // dropdownsData.forEach(d => {
     //   if (d.dropdown == data.dropdown || d.dropdown == data.dropdown.closest('.header__nav-dropdown.active') || d.dropdown.closest('.header__nav-dropdown.active') == data.dropdown.closest('.header__nav-dropdown.active')) {
     //     return;
     //   }
     //   d.dropdown.classList.remove('active');
     //   activeList.forEach(cl => d.list.classList.remove(cl));
+
     //   if (d.icon) {
     //     activeIcon.forEach(cl => d.icon.classList.remove(cl));
     //   }
     // });
-    // ///////////
+
     const rect = data.list.getBoundingClientRect();
+
     // Проверяем, выходит ли элемент за правую границу окна
     const isOverflowing = rect.right > window.innerWidth;
+
     data.dropdown.classList.add('active');
+
     if (isOverflowing) {
       if (data.dropdown != data.dropdown.offsetParent.closest('.header__nav-dropdown') && data.dropdown.offsetParent.closest('.header__nav-dropdown')) {
         [...activeList, '!left-auto', '!right-full'].forEach(cl => data.list.classList.add(cl));
@@ -43,38 +47,37 @@ dropdownsData.forEach(data => {
     } else {
       activeList.forEach(cl => data.list.classList.add(cl));
     }
+    data.list.classList.remove('pointer-events-none');
+
     if (data.icon) {
       activeIcon.forEach(cl => data.icon.classList.add(cl));
     }
   });
-  // console.log(data.dropdown);
 
-  data.dropdown.addEventListener('mouseout', ({ target }) => {
-    console.log(target);
+  data.dropdown.addEventListener('mouseleave', () => {
+    data.dropdown.classList.remove('active');
+    data.list.classList.add('pointer-events-none');
 
+    activeList.forEach(cl => data.list.classList.remove(cl));
+    if (data.icon) {
+      activeIcon.forEach(cl => data.icon.classList.remove(cl));
+    }
+  });
+});
+
+window.addEventListener('click', ({ target }) => {
+  const dropdown = document.querySelector('.header__nav-dropdown.active');
+
+  if (!dropdown || dropdown == target || target.closest('.header__nav-dropdown.active')) {
+    return;
+  } else {
     dropdownsData.forEach(d => {
       d.dropdown.classList.remove('active');
       activeList.forEach(cl => d.list.classList.remove(cl));
+
       if (d.icon) {
         activeIcon.forEach(cl => d.icon.classList.remove(cl));
       }
     });
-  });
-  // });
-
-  // window.addEventListener('click', ({ target }) => {
-  //   const dropdown = document.querySelector('.header__nav-dropdown.active');
-
-  //   if (!dropdown || dropdown == target || target.closest('.header__nav-dropdown.active')) {
-  //     return;
-  //   } else {
-  //     dropdownsData.forEach(d => {
-  //       d.dropdown.classList.remove('active');
-  //       activeList.forEach(cl => d.list.classList.remove(cl));
-
-  //       if (d.icon) {
-  //         activeIcon.forEach(cl => d.icon.classList.remove(cl));
-  //       }
-  //     });
-  //   }
+  }
 });
