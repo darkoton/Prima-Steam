f63c351a9af9f87ed045323188642dbe262c89e8
darkoto12 <darkoto12@gmail.com>
2024-10-20 18:51:02 +0300

    fixed size font

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,5 @@
 import fileinclude from "gulp-file-include";
 import webHtmlNosvg from "gulp-webp-html-nosvg";
-
 export const html = () => {
   return app.gulp.src(`${app.path.srcFolder}/html/*.html`, { soursemaps: true })
     .pipe(app.plugins.plumber(app.plugins.notify.onError({
@@ -8,7 +7,7 @@ export const html = () => {
       message: "Error: <%= error.message %>"
     })))
     .pipe(fileinclude())
-    .pipe(webHtmlNosvg())
+    // .pipe(webHtmlNosvg())
     .pipe(app.gulp.dest(`${app.path.buildFolder}/`))
     .pipe(app.plugins.browsersync.stream())
 }
