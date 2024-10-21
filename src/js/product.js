070717a6308c1b8f05b0bb92378053eb487e27c0
darkoto12 <darkoto12@gmail.com>
2024-10-21 11:05:25 +0300

    add fix

---
+++ src/js/product.js
@@ -0,0 +1,57 @@
+function productGallery() {
+  const galleries = document.querySelectorAll('[data-gallery]');
+
+  galleries.forEach(gallery => {
+    const imgs = gallery.querySelectorAll('[data-gallery-img]');
+    const result = gallery.querySelector('[data-gallery-result]');
+    const buttonLeft = gallery.querySelector('[data-gallery-left-button]');
+    const buttonRight = gallery.querySelector('[data-gallery-right-button]');
+    let active = 1;
+
+    imgs.forEach((img, index) => {
+      img.dataset.order = index + 1;
+
+      if (active == index + 1) {
+        img.classList.add('_active');
+      }
+
+      img.addEventListener('click', () => {
+        imgs.forEach(i => {
+          i.classList.remove('_active');
+        });
+
+        result.src = img.querySelector('img').src;
+        img.classList.add('_active');
+      });
+    });
+
+    buttonRight.addEventListener('click', () => {
+      const img = gallery.querySelector(`[data-order='${active + 1}']`);
+      if (img) {
+        active = active + 1;
+
+        imgs.forEach(i => {
+          i.classList.remove('_active');
+        });
+
+        result.src = img.querySelector('img').src;
+        img.classList.add('_active');
+      }
+    });
+    buttonLeft.addEventListener('click', () => {
+      const img = gallery.querySelector(`[data-order='${active - 1}']`);
+      if (img) {
+        active = active - 1;
+
+        imgs.forEach(i => {
+          i.classList.remove('_active');
+        });
+
+        result.src = img.querySelector('img').src;
+        img.classList.add('_active');
+      }
+    });
+  });
+}
+
+productGallery();
